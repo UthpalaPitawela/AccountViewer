367bf76b524562b5f4c84adfeb561a0d8f8be1dc
UthpalaP <UthpalaP@99x.io>
2023-05-24 10:11:49 +0530

    Routing issues resolved

--- webapi/Controllers/UsersController.cs
+++ webapi/Controllers/UsersController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using webapi.Models;
-using webapi.Services;
 using BCrypt.Net;
 namespace MyReadingList.WebAPI.Controllers
 {
